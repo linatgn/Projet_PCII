9ceba1b069612e597bfdf7d09d04c7f4a1c97de0
ElMegos <Mikail.ymz@hotmail.com>
2022-03-14 15:36:08 +0100

    Affichage HDV

---
+++ src/modele/unite/structure/batiment/Ferme.java
@@ -0,0 +1,15 @@
+package modele.unite.structure.batiment;
+
+import modele.Modele;
+
+public class Ferme extends Batiment {
+
+    public Ferme(int x, int y, Modele m) {
+        super(x, y,m);
+        x_texture = 5;
+        y_texture = 2;
+
+        LARGEUR = 1;
+        HAUTEUR = 1;
+    }
+}
